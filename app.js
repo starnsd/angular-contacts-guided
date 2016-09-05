e7c9cc092287f9819da8bcd81c44ee55e97208e1
Ryan Walsh <ryan@thehatrack.net>
2016-09-05 12:22:07 -0500

    post lecture

---
+++ app.js
@@ -0,0 +1,3 @@
+// set up new module
+// connect app to HTML
+angular.module( "AngularContacts", [] );
--- homeCtrl.js
+++ homeCtrl.js
@@ -1,3 +1,22 @@
+// set up a homeCtrl
+// connect it to index.html
+// test that its working
+
+angular.module( "AngularContacts" )
+.controller( "homeCtrl", function( $scope ) {
+  $scope.contacts = contacts;
+  $scope.showNewContactForm = false;
+
+  $scope.addContact = function( contact ) {
+    contact.picture = "http://placehold.it/32x32";
+    $scope.contacts.push( contact );
+
+    $scope.newContact = {};
+    $scope.showNewContactForm = false;
+  }
+
+} );
+
 var contacts = [
   {
     "_id": "57caeff3a4df71209304615a",
